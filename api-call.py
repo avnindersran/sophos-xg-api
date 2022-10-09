e9512ef9202d047433b87039cacc31a98d7cb966
avnindersran <102489856+avnindersran@users.noreply.github.com>
2022-10-09 09:40:41 -0700

    new file:   XML-request.xml 	new file:   api-call.py 	new file:   cerd.txt

---
+++ api-call.py
@@ -0,0 +1,15 @@
+import requests
+
+# Opening XG-Cerd file.
+with open("cerd.txt", "r") as cerd:
+    cerd = cerd.read()
+
+# Opening XML request file.
+with open("XML-request.xml", "r") as xml:
+    xmlrequest = xml.read()
+
+#API call
+response = requests.post(
+    "https://192.168.10.254:4445/webconsole/APIController?reqxml=" + cerd + xmlrequest, verify=False)
+print(response.status_code)
+print(response.text)
